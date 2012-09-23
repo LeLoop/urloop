a1b3c41b4582087a614b6437a3202d58c831570b
Marc Lagrange <rhaamo@sigpipe.me>
2012-09-23 18:30:01 +0200

    Fix default title if empty

--- urloop.rb
+++ urloop.rb
@@ -249,16 +249,13 @@ dputs "Logs to scan: #{@logs_to_scan.join(', ')}"
       # title is nil (fail), "" (no title, image), or a non empty string
       if title.nil?
         dputs "URL ignored #{url} return code is a 404 or other than 200 title '#{title}'"
+        title = "no title"
       end
       if !title.nil?
         title.gsub!("\n", "")
         title.gsub!("\r", "")
         title.strip!
       end
-
-      title = "no title" if title.empty?
-
-      valid = true if !title.nil?
 
       begin
         url = PostRank::URI.clean(url)
